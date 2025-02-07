b6ecfda78c38da39799060ee61279a7151b25f55
Ibtid <ibtidrahman@iut-dhaka.edu>
2025-02-07 14:08:06 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { FC, Fragment } from "react";
+
+import { BrowserRouter } from "react-router-dom";
+
+
+const App: FC = () => {
+  return (
+    <Fragment>
+      <BrowserRouter>
+       
+      </BrowserRouter>
+    </Fragment>
+  );
+};
+
+export default App;
